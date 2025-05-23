803b43a5c704f4614dd6b0d352724324df9a5da4
Xojimuxamedov <Dilshodxdxd@gmail.com>
2025-05-23 16:32:38 +0500

    add navbar and scss

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import "./navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="navbar-top">
+          <h2 className="navbar-top-title">
+            "FARM LOYIHA" mas`uliyati cheklangan jamiyati
+          </h2>
+          <div className="navbar-contact">
+            <a
+              class="headerup-first-mailText"
+              href="mailto:pharm.engineering@mail.ru?subject=Salom&body=Assalomu alaykum, sizga xabar yozyapman."
+              target="_blank"
+            >
+              Gmail
+            </a>
+            <a class="headerup-first-mailText" href="tel:+998996511111">
+              +998996511111
+            </a>
+            <a class="headerup-first-mailText" href="tel:+998998045040">
+              +998998045040
+            </a>
+          </div>
+        </div>
+        <div className="navbar-bottom">
+          <img src="../public/LOGO-loyiha.png" alt="Logo Farm Loyiha" className="navbar-logo"/>
+          <div className="navbar-links">
+            <div className="navbar-link">Bosh sahifa</div>
+            <div className="navbar-link">Biz haqimizda</div>
+            <div className="navbar-link">Xizmatlar</div>
+            <div className="navbar-link">Bog'lanish</div>
+            <div className="navbar-link">Yangiliklar</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
