e384f2916427211d12fe5d85589bd7060574a613
Xojimuxamedov <Dilshodxdxd@gmail.com>
2025-05-29 15:52:08 +0500

    Header contact

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import HomeCarousel from "./carousel/Carousel";
+import { Link } from "react-router";
 import "./header.scss";
 
 const Header = () => {
   return (
     <header>
       <HomeCarousel />
+      <div className="header-contact">
+        <div className="header-contact-inner">
+          <p className="header-contact-text">
+            Ko'proq malumot olish uchun biz bilan bog'laning
+          </p>
+          <Link className="header-contact-link" to={"/contact"}>Bog'lanish</Link>
+        </div>
+      </div>
     </header>
   );
 };
