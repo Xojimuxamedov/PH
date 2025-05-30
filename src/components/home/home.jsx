5b9f40ba82ddc0cd7810a14d91511b46c5f3ccad
Xojimuxamedov <Dilshodxdxd@gmail.com>
2025-05-30 16:12:06 +0500

    service first

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Header from './header/Header';
-
+import OurService from './ourService/OurService';
 const Home = () => {
     return (
         <>
             <Header/>
+            <OurService/>
         </>
     );
 };
---
+++ src/components/home/ourService/OurService.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./OurService.scss";
+
+const OurService = () => {
+  return (
+    <section className="service">
+      <div className="service-inner">
+        <h3 className="service-title">Bizning hizmatlar</h3>
+        <div className="service-links">
+          <div className="service-link div1">1</div>
+          <div className="service-link div2">2</div>
+          <div className="service-link div3">3</div>
+          <div className="service-link div4">4</div>
+          <div className="service-link div5">5</div>
+          <div className="service-link div6">6</div>
+          <div className="service-link div7">7</div>
+          <div className="service-link div8">8</div>
+          <div className="service-link div9">9</div>
+          <div className="service-link div10">10</div>
+          <div className="service-link div11">11</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default OurService;
